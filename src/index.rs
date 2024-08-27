a4624ca84c70b72d92fec6c2949acdf20c5f0d71
Claus Guttesen <kometen@gmail.com>
2024-08-27 20:03:58 +0200

    Moved index to own file.

---
+++ src/index.rs
@@ -0,0 +1,38 @@
+use std::path::PathBuf;
+use std::sync::atomic::Ordering;
+use askama::Template;
+use rocket::fs::NamedFile;
+use rocket::response::status::NotFound;
+use rocket::State;
+use crate::PasswordAttributes;
+
+#[derive(Template)]
+#[template(path = "index.html")]
+pub struct IndexTemplate {
+    name: String,
+    password_count_value: u8,
+    password_length_value: u8,
+}
+
+#[get("/")]
+pub async fn root(password_count: &State<PasswordAttributes>) -> Result<IndexTemplate, NotFound<String>> {
+
+    println!("Initial password count: {}", password_count.count.load(Ordering::Relaxed));
+
+    let template = IndexTemplate {
+        name: "World".to_string(),
+        password_count_value: password_count.count.load(Ordering::Relaxed),
+        password_length_value: password_count.length.load(Ordering::Relaxed),
+    };
+
+    let response = template;
+    Ok(response)
+}
+
+#[get("/<path..>")]
+pub async fn static_files(path: PathBuf) -> Result<NamedFile, NotFound<String>> {
+    let path = PathBuf::from("site").join(path);
+    NamedFile::open(path)
+        .await
+        .map_err(|e| NotFound(e.to_string()))
+}
--- src/main.rs
+++ src/main.rs
@@ -3,52 +3,18 @@
 
 mod password_counter;
 mod password_length;
+mod index;
 
 #[macro_use] extern crate rocket;
 
-use std::path::PathBuf;
-use std::sync::atomic::{AtomicU8, Ordering};
-use rocket::response::status::NotFound;
-use askama_rocket::{Template};
-use rocket::fs::NamedFile;
-use rocket::State;
+use std::sync::atomic::{AtomicU8};
+use crate::index::{root, static_files};
 use crate::password_counter::{decrement_password_count, increment_password_count};
 use crate::password_length::{decrement_password_length, increment_password_length};
 
 struct PasswordAttributes {
     count: AtomicU8,
     length: AtomicU8,
-}
-
-#[derive(Template)]
-#[template(path = "index.html")]
-struct IndexTemplate {
-    name: String,
-    password_count_value: u8,
-    password_length_value: u8,
-}
-
-#[get("/")]
-async fn root(password_count: &State<PasswordAttributes>) -> Result<IndexTemplate, NotFound<String>> {
-
-    println!("Initial password count: {}", password_count.count.load(Ordering::Relaxed));
-
-    let template = IndexTemplate {
-        name: "World".to_string(),
-        password_count_value: password_count.count.load(Ordering::Relaxed),
-        password_length_value: password_count.length.load(Ordering::Relaxed),
-    };
-
-    let response = template;
-    Ok(response)
-}
-
-#[get("/<path..>")]
-async fn static_files(path: PathBuf) -> Result<NamedFile, NotFound<String>> {
-    let path = PathBuf::from("site").join(path);
-    NamedFile::open(path)
-        .await
-        .map_err(|e| NotFound(e.to_string()))
 }
 
 #[launch]
