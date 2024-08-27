fcc18312e8de637022c88a60589506bcf8105b76
Claus Guttesen <kometen@gmail.com>
2024-08-27 15:38:01 +0200

    Use rocket::State.

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,37 @@
+// https://kojinglick.com/using-htmx-with-rust-quickstart
+// https://medium.com/perimeterx/late-night-confessions-building-a-website-using-rust-rocket-diesel-and-askama-part-3-3f9b7d511bde
+
 #[macro_use] extern crate rocket;
 
 use std::path::PathBuf;
+use std::sync::atomic::{AtomicU8, Ordering};
 use rocket::response::status::NotFound;
 use askama_rocket::Template;
 use rocket::fs::NamedFile;
+use rocket::State;
+
+struct PasswordAttributes {
+    count: AtomicU8,
+}
 
 #[derive(Template)]
 #[template(path = "index.html")]
 struct IndexTemplate {
     name: String,
+    password_count_value: u8,
 }
 
 #[get("/")]
-async fn root() -> Result<IndexTemplate, NotFound<String>> {
+async fn root(password_count: &State<PasswordAttributes>) -> Result<IndexTemplate, NotFound<String>> {
     let template = IndexTemplate {
         name: "World".to_string(),
+        password_count_value: password_count.count.load(Ordering::Relaxed),
     };
 
     let response = template;
     Ok(response)
 }
+
 
 #[get("/<path..>")]
 async fn static_files(path: PathBuf) -> Result<NamedFile, NotFound<String>> {
@@ -31,5 +43,7 @@ async fn static_files(path: PathBuf) -> Result<NamedFile, NotFound<String>> {
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![root, static_files])
+    rocket::build()
+        .manage(PasswordAttributes { count: AtomicU8::new(5)})
+        .mount("/", routes![root, static_files])
 }
